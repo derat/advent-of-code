0cc35b6cef2f2611e750749a4d690dc404ec8046
Daniel Erat <dan@erat.org>
2020-12-07 20:52:56 -0400

    2: Part 2.

--- 2/main.go
+++ 2/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	re := regexp.MustCompile(`^(\d+)-(\d+) ([a-z]): ([a-z]+)$`)
-	valid := 0
+	var valid1, valid2 int
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
 		m := re.FindStringSubmatch(sc.Text())
@@ -21,13 +21,17 @@ func main() {
 		min, _ := strconv.Atoi(m[1])
 		max, _ := strconv.Atoi(m[2])
 		ch, pw := m[3], m[4]
-		n := len(pw) - len(strings.ReplaceAll(pw, ch, ""))
-		if n >= min && n <= max {
-			valid++
+
+		if n := len(pw) - len(strings.ReplaceAll(pw, ch, "")); n >= min && n <= max {
+			valid1++
+		}
+		if min >= 1 && min <= len(pw) && max >= 1 && max <= len(pw) &&
+			((pw[min-1] == ch[0]) != (pw[max-1] == ch[0])) {
+			valid2++
 		}
 	}
 	if sc.Err() != nil {
 		panic(sc.Err())
 	}
-	fmt.Println(valid)
+	fmt.Println(valid1, valid2)
 }
