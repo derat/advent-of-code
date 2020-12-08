fb33a27c221879d97963a720915c1e4228bb891d
Daniel Erat <dan@erat.org>
2020-12-07 20:40:16 -0400

    1: Part 2.

--- 1/main.go
+++ 1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -17,12 +18,21 @@ func main() {
 		}
 		rem := total - n
 		if _, ok := seen[rem]; ok {
-			println(n * rem)
-			os.Exit(0)
+			fmt.Printf("%d + %d = %d, %d * %d = %d\n", n, rem, total, n, rem, n*rem)
 		}
 		seen[n] = struct{}{}
 	}
 	if sc.Err() != nil {
 		panic(sc.Err())
+	}
+
+	for n := range seen {
+		r1 := total - n
+		for m := range seen {
+			r2 := r1 - m
+			if _, ok := seen[r2]; ok {
+				fmt.Printf("%d + %d + %d = %d, %d * %d * %d = %d\n", n, m, r2, total, n, m, r2, n*m*r2)
+			}
+		}
 	}
 }
