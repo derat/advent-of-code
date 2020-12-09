ebba2baa95feb505517ace40d807a744768fdf74
Daniel Erat <dan@erat.org>
2020-12-09 11:27:49 -0400

    9: Part 2.

--- 9/main.go
+++ 9/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
@@ -11,6 +13,7 @@ func main() {
 	ring := make([]int, 0, psize)
 	idx := 0 // latest element in ring
 	lookup := make(map[int]int8, psize)
+	var all []int // so much for efficiency
 
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
@@ -33,7 +36,34 @@ func main() {
 				}
 			}
 			if !found {
-				println(n)
+				fmt.Println(n)
+
+				i, j := 0, 1
+				total := all[i] + all[j]
+			Find:
+				for j < len(all) {
+					switch {
+					case total == n:
+						min, max := math.MaxInt32, math.MinInt32
+						for _, v := range all[i : j+1] {
+							if v < min {
+								min = v
+							}
+							if v > max {
+								max = v
+							}
+						}
+						fmt.Println(min+max, all[i:j+1])
+						break Find
+					case total > n && i < j-1:
+						total -= all[i]
+						i++
+					default:
+						j++
+						total += all[j]
+					}
+				}
+				break
 			}
 		}
 
@@ -51,6 +81,7 @@ func main() {
 			ring[idx] = n
 		}
 		lookup[n]++
+		all = append(all, n)
 	}
 	if sc.Err() != nil {
 		panic(sc.Err())
