2cc2e666cd6422ba475b126b7807aace9ea42c44
Daniel Erat <dan@erat.org>
2020-12-11 10:30:35 -0400

    11: Part 2.

--- 11/main.go
+++ 11/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"fmt"
 	"log"
 	"os"
 )
@@ -21,9 +20,7 @@ func main() {
 		log.Fatal(sc.Err())
 	}
 
-	const flip = 4 // number of adj occ seats to switch to empty
-
-	occ := func(row, col int) int {
+	occ := func(rows [][]byte, row, col int) int {
 		if row < 0 || col < 0 || row >= len(rows) || col >= len(rows[row]) {
 			return 0
 		}
@@ -33,27 +30,54 @@ func main() {
 		return 1
 	}
 
+	look := func(rows [][]byte, row, col, dr, dc int) int {
+		for r, c := row+dr, col+dc; r >= 0 && r < len(rows) && c >= 0 && c < len(rows[r]); r, c = r+dr, c+dc {
+			switch rows[r][c] {
+			case '#':
+				return 1
+			case 'L':
+				return 0
+			}
+		}
+		return 0
+	}
+
+	// Part 1
+	println(run(rows, func(rows [][]byte, row, col int) int {
+		return occ(rows, row-1, col-1) + occ(rows, row-1, col) + occ(rows, row-1, col+1) +
+			occ(rows, row, col-1) + occ(rows, row, col+1) +
+			occ(rows, row+1, col-1) + occ(rows, row+1, col) + occ(rows, row+1, col+1)
+	}, 4))
+
+	// Part 2
+	println(run(rows, func(rows [][]byte, row, col int) int {
+		return look(rows, row, col, -1, -1) + look(rows, row, col, -1, 0) + look(rows, row, col, -1, 1) +
+			look(rows, row, col, 0, -1) + look(rows, row, col, 0, 1) +
+			look(rows, row, col, 1, -1) + look(rows, row, col, 1, 0) + look(rows, row, col, 1, 1)
+	}, 5))
+}
+
+type countFunc func(rows [][]byte, row, col int) int
+
+func run(rows [][]byte, f countFunc, flip int) int {
 	for {
 		newRows := make([][]byte, len(rows))
 		for i := range rows {
 			newRows[i] = bytes.Repeat([]byte{0}, len(rows[i]))
 			for j := range rows[i] {
 				ch := rows[i][j]
-				if ch != '.' {
-					adj := occ(i-1, j-1) + occ(i-1, j) + occ(i-1, j+1) +
-						occ(i, j-1) + occ(i, j+1) +
-						occ(i+1, j-1) + occ(i+1, j) + occ(i+1, j+1)
-					if ch == 'L' {
-						if adj == 0 {
-							ch = '#'
-						}
-					} else if ch == '#' {
-						if adj >= flip {
-							ch = 'L'
-						}
-					} else {
-						log.Fatalf("invalid state %q", ch)
+				switch ch {
+				case '.': // floor
+				case 'L': // empty
+					if n := f(rows, i, j); n == 0 {
+						ch = '#'
 					}
+				case '#': // occupied
+					if n := f(rows, i, j); n >= flip {
+						ch = 'L'
+					}
+				default:
+					log.Fatalf("invalid state %q", ch)
 				}
 				newRows[i][j] = ch
 			}
@@ -70,8 +94,7 @@ func main() {
 					}
 				}
 			}
-			fmt.Println(cnt)
-			break
+			return cnt
 		}
 
 		rows = newRows
