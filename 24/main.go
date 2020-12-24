753310777425b9cf4c9ff092565292383b1ab83c
Daniel Erat <dan@erat.org>
2020-12-24 10:14:17 -0400

    24: Part 2.

--- 24/main.go
+++ 24/main.go
@@ -4,13 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strings"
 )
 
 func main() {
-	black := make(map[string]struct{})
+	black := make(map[uint64]struct{})
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
 		ln := strings.TrimSpace(sc.Text())
@@ -18,9 +17,8 @@ func main() {
 			continue
 		}
 
-		var x, y int
+		var x, y int32
 		for i := 0; i < len(ln); i++ {
-			ay := int(math.Abs(float64(y)))
 			switch ln[i] {
 			case 'e':
 				x++
@@ -29,9 +27,9 @@ func main() {
 			case 'n':
 				switch ln[i+1] {
 				case 'e':
-					x += ay % 2
+					x += ex(y)
 				case 'w':
-					x -= (ay + 1) % 2
+					x += wx(y)
 				default:
 					log.Fatalf("Bad line %q", ln)
 				}
@@ -40,9 +38,9 @@ func main() {
 			case 's':
 				switch ln[i+1] {
 				case 'e':
-					x += ay % 2
+					x += ex(y)
 				case 'w':
-					x -= (ay + 1) % 2
+					x += wx(y)
 				default:
 					log.Fatalf("Bad line %q", ln)
 				}
@@ -53,7 +51,7 @@ func main() {
 			}
 		}
 
-		key := fmt.Sprintf("%d|%d", x, y)
+		key := makeKey(x, y)
 		if _, ok := black[key]; ok {
 			delete(black, key) // flip to white
 		} else {
@@ -64,4 +62,89 @@ func main() {
 		log.Fatal(sc.Err())
 	}
 	fmt.Println(len(black))
+
+	const days = 100
+	for i := 0; i < days; i++ {
+		nextBlack := make(map[uint64]struct{}) // black tiles for next iteration
+		doneWhite := make(map[uint64]struct{}) // white tiles already updated
+
+		// Checks the white tile at (x, y).
+		updateWhite := func(x, y int32) {
+			key := makeKey(x, y)
+			if _, ok := black[key]; ok {
+				return // not white
+			}
+			if _, ok := doneWhite[key]; ok {
+				return // already handled
+			}
+			// White with 2 black adj is flipped.
+			if countAdj(black, x, y) == 2 {
+				nextBlack[key] = struct{}{}
+			}
+			doneWhite[key] = struct{}{}
+		}
+
+		for key := range black {
+			x, y := unpackKey(key)
+			// Black with 0 or >2 black adj is flipped, so only preserve if 1 or 2.
+			if cnt := countAdj(black, x, y); cnt == 1 || cnt == 2 {
+				nextBlack[key] = struct{}{}
+			}
+
+			// Check neighbors.
+			updateWhite(x+1, y)       // e
+			updateWhite(x-1, y)       // w
+			updateWhite(x+ex(y), y+1) // ne
+			updateWhite(x+wx(y), y+1) // nw
+			updateWhite(x+ex(y), y-1) // se
+			updateWhite(x+wx(y), y-1) // sw
+		}
+
+		black = nextBlack
+	}
+
+	fmt.Println(len(black))
+}
+
+func makeKey(x, y int32) uint64 {
+	return uint64(x)<<32 | (uint64(y) & 0xffffffff)
+}
+
+func unpackKey(k uint64) (x, y int32) {
+	x = int32((k >> 32) & 0xffffffff)
+	y = int32(k & 0xffffffff)
+	return
+}
+
+// X offset for NE or SE move starting from y.
+func ex(y int32) int32 {
+	if y < 0 {
+		y = -y
+	}
+	return int32(y % 2)
+}
+
+// X offset for NW or SW move starting from y.
+func wx(y int32) int32 {
+	if y < 0 {
+		y = -y
+	}
+	return -int32((y + 1) % 2)
+}
+
+// Returns number of black tiles adjacent to (x, y).
+func countAdj(m map[uint64]struct{}, x, y int32) int {
+	var cnt int
+	check := func(x, y int32) {
+		if _, ok := m[makeKey(x, y)]; ok {
+			cnt++
+		}
+	}
+	check(x+1, y)       // e
+	check(x-1, y)       // w
+	check(x+ex(y), y+1) // ne
+	check(x+wx(y), y+1) // nw
+	check(x+ex(y), y-1) // se
+	check(x+wx(y), y-1) // sw
+	return cnt
 }
