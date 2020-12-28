e2bf70127452aec3234996c313891c4263c4ea05
Daniel Erat <dan@erat.org>
2020-12-28 12:24:54 -0400

    2015/04: Part 2.

--- 2015/04/main.go
+++ 2015/04/main.go
@@ -13,13 +13,22 @@ func main() {
 	const zeros = 5               // number of leading zeros
 	want := make([]byte, zeros/2) // full zero bytes
 
+	done := false // already printed a solution for part1
+
 	pre := lib.InputLines("2015/4")[0]
 	for i := 0; true; i++ {
 		s := pre + strconv.Itoa(i)
 		hash := md5.Sum([]byte(s))
 		if bytes.HasPrefix(hash[:], want) && (zeros%2 == 0 || hash[zeros/2] < 16) {
-			fmt.Println(i)
-			break
+			if !done {
+				fmt.Println(i)
+				done = true
+			}
+			// Part 2: Check the full last byte. (We know zeroes is odd.)
+			if hash[zeros/2] == 0 {
+				fmt.Println(i)
+				break
+			}
 		}
 	}
 }
