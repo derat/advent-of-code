932c9cd24db5c9f27740a9d321935edc9ad31673
Daniel Erat <dan@erat.org>
2020-12-28 15:45:48 -0400

    2015/08: Part 2.

--- 2015/08/main.go
+++ 2015/08/main.go
@@ -10,13 +10,25 @@ import (
 func main() {
 	hexRegexp := regexp.MustCompile(`^\\x[0-9a-f]{2}`)
 	charRegexp := regexp.MustCompile(`^[^\\]`)
-	var lit, mem int
+	var lit, mem, enc int
 	for _, ln := range lib.InputLines("2015/8") {
+		// Part 1
 		var inner string
 		lib.Extract(ln, `^"(.*)"$`, &inner)
 		toks := lib.Tokenize(inner, `\\`, `\"`, hexRegexp, charRegexp)
 		lit += len(ln)
 		mem += len(toks)
+
+		// Part 2
+		enc += 2 // start/end quote
+		for _, ch := range ln {
+			if ch == '"' || ch == '\\' {
+				enc += 2
+			} else {
+				enc++
+			}
+		}
 	}
 	fmt.Println(lit - mem)
+	fmt.Println(enc - lit)
 }
