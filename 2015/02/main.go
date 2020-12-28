b69df6064be9bc07e633e4f87a3ec57e877cc0c2
Daniel Erat <dan@erat.org>
2020-12-28 11:49:01 -0400

    2015/02: Part 2.

--- 2015/02/main.go
+++ 2015/02/main.go
@@ -7,13 +7,19 @@ import (
 )
 
 func main() {
-	sum := 0
+	var paper, ribbon int
 	for _, ln := range lib.InputLines("2015/2") {
 		var l, w, h int
 		lib.Extract(ln, `^(\d+)x(\d+)x(\d+)$`, &l, &w, &h)
+
 		lw, wh, hl := l*w, w*h, h*l
 		min := lib.Min(lw, wh, hl)
-		sum += 2*lw + 2*wh + 2*hl + min
+		paper += 2*lw + 2*wh + 2*hl + min
+
+		dist := lib.Min(2*l+2*w, 2*w+2*h, 2*h+2*l)
+		vol := l * w * h
+		ribbon += dist + vol
 	}
-	fmt.Println(sum)
+	fmt.Println(paper)
+	fmt.Println(ribbon)
 }
