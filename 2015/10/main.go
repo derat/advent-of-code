987a1144e7498e99ac4d60a11a93b811033585d4
Daniel Erat <dan@erat.org>
2020-12-28 17:38:57 -0400

    2015/10: Part 2.

--- 2015/10/main.go
+++ 2015/10/main.go
@@ -7,25 +7,40 @@ import (
 )
 
 func main() {
-	seq := lib.ExtractDigits(lib.InputLines("2015/10")[0])
-	const turns = 40
-	for turn := 0; turn < turns; turn++ {
-		var newSeq []int
-		var cur, cnt int
-		for i, v := range seq {
-			if i == 0 {
-				cur = v
-			}
-			if v == cur {
-				cnt++
-			} else {
-				newSeq = append(newSeq, cnt, cur)
-				cur = v
-				cnt = 1
-			}
-		}
-		newSeq = append(newSeq, cnt, cur)
-		seq = newSeq
+	var seq []uint8
+	for _, v := range lib.ExtractDigits(lib.InputLines("2015/10")[0]) {
+		seq = append(seq, uint8(v))
 	}
-	fmt.Println(len(seq))
+
+	const (
+		turns  = 40
+		turns2 = 50
+	)
+	for turn := 1; turn <= turns2; turn++ {
+		seq = say(seq)
+		if turn == turns {
+			fmt.Println(len(seq))
+		} else if turn == turns2 {
+			fmt.Println(len(seq))
+			break
+		}
+	}
+}
+
+func say(seq []uint8) []uint8 {
+	var newSeq []uint8
+	var cur, cnt uint8
+	for i, v := range seq {
+		if i == 0 {
+			cur = v
+		}
+		if v == cur {
+			cnt++
+		} else {
+			newSeq = append(newSeq, cnt, cur)
+			cur = v
+			cnt = 1
+		}
+	}
+	return append(newSeq, cnt, cur)
 }
