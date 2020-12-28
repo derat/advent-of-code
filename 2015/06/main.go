e123ce3e0d3ac6e1f6d1b388842612bee6f31987
Daniel Erat <dan@erat.org>
2020-12-28 14:00:50 -0400

    2015/06: Part 2.

--- 2015/06/main.go
+++ 2015/06/main.go
@@ -8,9 +8,17 @@ import (
 
 func main() {
 	const dim = 1000
+
+	// Part 1
 	on := make([][]bool, dim)
 	for r := range on {
 		on[r] = make([]bool, dim)
+	}
+
+	// Part 2
+	brt := make([][]uint16, dim)
+	for r := range brt {
+		brt[r] = make([]uint16, dim)
 	}
 
 	for _, ln := range lib.InputLines("2015/6") {
@@ -24,10 +32,15 @@ func main() {
 				switch op {
 				case "turn on":
 					on[r][c] = true
+					brt[r][c]++
 				case "turn off":
 					on[r][c] = false
+					if brt[r][c] > 0 {
+						brt[r][c]--
+					}
 				case "toggle":
 					on[r][c] = !on[r][c]
+					brt[r][c] += 2
 				}
 			}
 		}
@@ -42,4 +55,12 @@ func main() {
 		}
 	}
 	fmt.Println(cnt)
+
+	var cnt2 int64
+	for _, r := range brt {
+		for _, v := range r {
+			cnt2 += int64(v)
+		}
+	}
+	fmt.Println(cnt2)
 }
