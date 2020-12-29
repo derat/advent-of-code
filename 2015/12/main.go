60fcb469c8ad8296e2993b30b2945ad3a236fdbc
Daniel Erat <dan@erat.org>
2020-12-28 22:46:57 -0400

    2015/12: Part 2.

--- 2015/12/main.go
+++ 2015/12/main.go
@@ -16,11 +16,39 @@ func main() {
 	var sum int
 	stringRegexp := regexp.MustCompile(`^"[^"]*"`)
 	numRegexp := regexp.MustCompile(`^-?\d+`)
+	stack := []frame{{}}
 	for _, tok := range lib.Tokenize(ln, "[", "]", "{", "}", ",", ":", stringRegexp, numRegexp) {
-		v, err := strconv.Atoi(tok)
-		if err == nil {
-			sum += v
+		switch tok {
+		case "[", "{":
+			stack = append(stack, frame{tok: tok})
+		case "]", "}":
+			if top := &stack[len(stack)-1]; !top.red {
+				stack[len(stack)-2].sum += top.sum
+			}
+			stack = stack[:len(stack)-1]
+		case `"red"`:
+			// Laziness alert: my input never contains "red" as a property name, so
+			// just look for "red" tokens while an object is at the top of the stack.
+			if top := &stack[len(stack)-1]; top.tok == "{" {
+				top.red = true
+			}
+		default:
+			v, err := strconv.Atoi(tok)
+			if err == nil {
+				sum += v
+				stack[len(stack)-1].sum += v
+			}
 		}
+
 	}
 	fmt.Println(sum)
+
+	lib.AssertEq(len(stack), 1)
+	fmt.Println(stack[0].sum)
+}
+
+type frame struct {
+	tok string
+	sum int
+	red bool
 }
