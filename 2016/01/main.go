fa3953c7aad91d185662642bf3e9b518b0092884
Daniel Erat <dan@erat.org>
2021-01-01 12:10:44 -0400

    2016/01: Part 2.

--- 2016/01/main.go
+++ 2016/01/main.go
@@ -10,6 +10,13 @@ import (
 func main() {
 	var x, y int
 	dx, dy := 0, 1
+
+	var twice bool
+	var tx, ty int
+	visited := map[uint64]struct{}{
+		lib.PackInts([]int{0, 0}, 32): struct{}{},
+	}
+
 	for _, s := range strings.Split(lib.InputLines("2016/1")[0], ", ") {
 		var dir string
 		var dist int
@@ -22,7 +29,26 @@ func main() {
 			dx = mult * -dy
 			dy = 0
 		}
-		x, y = x+dist*dx, y+dist*dy
+
+		for i := 0; i < dist; i++ {
+			x, y = x+dx, y+dy
+
+			// Part 2: Find first location visited twice.
+			// I was initially thrown because I thought that only ending points count as visits.
+			// The problem actually wants the first point where the path intersects itself.
+			if !twice {
+				key := lib.PackInts([]int{x, y}, 32)
+				if _, ok := visited[key]; ok {
+					tx, ty = x, y
+					twice = true
+				} else {
+					visited[key] = struct{}{}
+				}
+			}
+		}
 	}
+
 	fmt.Println(lib.Abs(x) + lib.Abs(y))
+	lib.Assert(twice)
+	fmt.Println(lib.Abs(tx) + lib.Abs(ty))
 }
