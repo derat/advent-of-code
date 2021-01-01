992e40ff1e4b4faf2a8671cc22e7364a5fb2e9d9
Daniel Erat <dan@erat.org>
2021-01-01 11:56:37 -0400

    2016/01: Part 1. Also add lib.Abs.

---
+++ 2016/01/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/derat/advent-of-code/lib"
+)
+
+func main() {
+	var x, y int
+	dx, dy := 0, 1
+	for _, s := range strings.Split(lib.InputLines("2016/1")[0], ", ") {
+		var dir string
+		var dist int
+		lib.Extract(s, `^([LR])(\d+)$`, &dir, &dist)
+		mult := 1
+		if dir == "R" {
+			mult = -1
+		}
+		if dx != 0 {
+			dy = mult * dx
+			dx = 0
+		} else {
+			dx = mult * -dy
+			dy = 0
+		}
+		x, y = x+dist*dx, y+dist*dy
+	}
+	fmt.Println(lib.Abs(x) + lib.Abs(y))
+}
--- lib/math.go
+++ lib/math.go
@@ -36,3 +36,11 @@ func Sum(vals ...int) int {
 	}
 	return sum
 }
+
+// Abs returns the absolute value of v.
+func Abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
