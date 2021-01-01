00801d8c3e89a658920c721c89644d60e42eb58b
Daniel Erat <dan@erat.org>
2021-01-01 14:14:36 -0400

    2016/03: Part 2.

--- 2016/03/main.go
+++ 2016/03/main.go
@@ -7,21 +7,40 @@ import (
 )
 
 func main() {
-	var possible int
+	var lines [][]int
 	for _, ln := range lib.InputLines("2016/3") {
-		sides := lib.ExtractInts(ln)
-		lib.AssertEq(len(sides), 3)
-		sum := lib.Sum(sides...)
-		valid := true
-		for _, s := range sides {
-			if sum-s <= s { // or sum <= 2*s
-				valid = false
-				break
-			}
-		}
-		if valid {
-			possible++
+		lines = append(lines, lib.ExtractInts(ln))
+	}
+
+	// Part 1: Each line specifies triangle's sides
+	var poss int
+	for _, sides := range lines {
+		if valid(sides...) {
+			poss++
 		}
 	}
-	fmt.Println(possible)
+	fmt.Println(poss)
+
+	// Part 2: Each group of 3 going down each column specifies triangle's sides
+	var poss2 int
+	lib.AssertEq(len(lines)%3, 0)
+	for i := 0; i < len(lines); i += 3 {
+		for j := 0; j < len(lines[i]); j++ {
+			if valid(lines[i][j], lines[i+1][j], lines[i+2][j]) {
+				poss2++
+			}
+		}
+	}
+	fmt.Println(poss2)
+}
+
+func valid(sides ...int) bool {
+	lib.AssertEq(len(sides), 3)
+	sum := lib.Sum(sides...)
+	for _, s := range sides {
+		if sum-s <= s { // or sum <= 2*s
+			return false
+		}
+	}
+	return true
 }
