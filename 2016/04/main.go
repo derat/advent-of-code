c7694b9c4fd302e779ef0e52f40dbf6585857a30
Daniel Erat <dan@erat.org>
2021-01-01 14:52:45 -0400

    2016/04: Part 2.

--- 2016/04/main.go
+++ 2016/04/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/derat/advent-of-code/lib"
 )
 
 func main() {
-	var sum int
+	var sum, id2 int
 	for _, ln := range lib.InputLines("2016/4") {
 		var name, checksum string
 		var id int
@@ -27,7 +28,26 @@ func main() {
 		})
 		if string(letters[:5]) == checksum {
 			sum += id
+
+			// Part 2: Decrypt room names.
+			dec := []byte(name)
+			for i, ch := range dec {
+				if ch == '-' {
+					dec[i] = ' '
+				} else {
+					dec[i] = byte('a' + (int(ch-'a')+id)%26)
+				}
+			}
+
+			// This felt ugly. The question just asks "What is the sector ID of the room where North
+			// Pole objects are stored?", so it seems like you're supposed to print the decoded
+			// names first and then manually scan through them for one that seems similar to the
+			// description. For my input, it's "northpole object storage".
+			if s := strings.ToLower(string(dec)); strings.Contains(s, "north") && strings.Contains(s, "pole") {
+				id2 = id
+			}
 		}
 	}
 	fmt.Println(sum)
+	fmt.Println(id2)
 }
