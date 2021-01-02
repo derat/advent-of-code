2965faf95929e8694556c11edf8835da7c5e4909
Daniel Erat <dan@erat.org>
2021-01-02 10:52:05 -0400

    2016/09: Part 2.

--- 2016/09/main.go
+++ 2016/09/main.go
@@ -9,19 +9,32 @@ import (
 
 func main() {
 	in := lib.InputLines("2016/9")[0]
-	markerRegexp := regexp.MustCompile(`^\((\d+)x(\d+)\)`)
-	var i, cnt int
-	for i < len(in) {
-		s := in[i:]
+
+	cnt := expand(in, false)
+	fmt.Println(cnt)
+
+	cnt2 := expand(in, true)
+	fmt.Println(cnt2)
+}
+
+var markerRegexp = regexp.MustCompile(`^\((\d+)x(\d+)\)`)
+
+func expand(s string, recurse bool) (expanded int64) {
+	for i := 0; i < len(s); {
+		s := s[i:]
 		if m := markerRegexp.FindString(s); m != "" {
 			var length, times int
 			lib.Extract(s, markerRegexp.String(), &length, &times)
-			cnt += length * times
+			if recurse {
+				expanded += int64(times) * expand(s[len(m):len(m)+length], true)
+			} else {
+				expanded += int64(times * length)
+			}
 			i += len(m) + length
 		} else {
-			cnt++
+			expanded++
 			i++
 		}
 	}
-	fmt.Println(cnt)
+	return expanded
 }
