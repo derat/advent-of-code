521a81bf3f995c291c71ac5127b16d1bc4af5e27
Daniel Erat <dan@erat.org>
2021-01-02 18:54:17 -0400

    2016/12: Part 1.

---
+++ 2016/12/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/derat/advent-of-code/lib"
+)
+
+func main() {
+	var ins []instr
+	for _, ln := range lib.InputLines("2016/12") {
+		var in instr
+		lib.Extract(ln, `^(cpy|inc|dec|jnz) (?:([a-d])|(-?\d+))(?: ([a-d])| (-?\d+))?$`,
+			&in.op, &in.r1, &in.v1, &in.r2, &in.v2)
+		ins = append(ins, in)
+	}
+
+	a, _, _, _ := run(ins)
+	fmt.Println(a)
+}
+
+type instr struct {
+	op     string
+	r1, r2 string
+	v1, v2 int64
+}
+
+func run(ins []instr) (a, b, c, d int64) {
+	reg := func(n string) *int64 {
+		switch n {
+		case "a":
+			return &a
+		case "b":
+			return &b
+		case "c":
+			return &c
+		case "d":
+			return &d
+		default:
+			panic(fmt.Sprintf("Invalid register %q", n))
+		}
+	}
+
+	var ip int64
+	for ip >= 0 && ip < int64(len(ins)) {
+		in := &ins[ip]
+		switch in.op {
+		case "cpy":
+			if in.r1 != "" {
+				*reg(in.r2) = *reg(in.r1)
+			} else {
+				*reg(in.r2) = in.v1
+			}
+			ip++
+		case "inc":
+			*reg(in.r1)++
+			ip++
+		case "dec":
+			*reg(in.r1)--
+			ip++
+		case "jnz":
+			v := in.v1
+			if in.r1 != "" {
+				v = *reg(in.r1)
+			}
+			if v != 0 {
+				ip += in.v2
+			} else {
+				ip++
+			}
+		default:
+			panic(fmt.Sprintf("Invalid op %q", in.op))
+		}
+	}
+
+	return a, b, c, d
+}
