2506d93e14b2e7a0125e13d9df1e2945f131f77f
Daniel Erat <dan@erat.org>
2021-01-02 10:39:24 -0400

    2016/09: Part 1.

--- 2016/09/main.go
+++ 2016/09/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/derat/advent-of-code/lib"
 )
 
 func main() {
-	for _, ln := range lib.InputLines("2016/9") {
-		fmt.Println(ln)
+	in := lib.InputLines("2016/9")[0]
+	markerRegexp := regexp.MustCompile(`^\((\d+)x(\d+)\)`)
+	var i, cnt int
+	for i < len(in) {
+		s := in[i:]
+		if m := markerRegexp.FindString(s); m != "" {
+			var length, times int
+			lib.Extract(s, markerRegexp.String(), &length, &times)
+			cnt += length * times
+			i += len(m) + length
+		} else {
+			cnt++
+			i++
+		}
 	}
+	fmt.Println(cnt)
 }
