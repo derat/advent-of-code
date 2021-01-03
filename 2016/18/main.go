c31bd9f623a7dacc2930fb1c8e35748d3ef1b1d8
Daniel Erat <dan@erat.org>
2021-01-03 14:35:58 -0400

    2016/18: Part 2.

--- 2016/18/main.go
+++ 2016/18/main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	const (
-		nrows = 40
+		nrows  = 40
+		nrows2 = 400000
 
 		trap = '^'
 		safe = '.'
@@ -20,21 +21,35 @@ func main() {
 
 	first := lib.InputLinesBytes("2016/18", '.', '^')[0]
 
-	rows := make([][]byte, nrows)
-	rows[0] = first
-	for r := 1; r < nrows; r++ {
-		rows[r] = make([]byte, len(rows[0]))
-		prev := rows[r-1]
-		for i := range rows[r] {
+	cnt := lib.CountBytesFull([][]byte{first}, '.')
+	prev := first
+	for r := 1; r < nrows2; r++ {
+		// Part 1
+		if r == nrows {
+			fmt.Println(cnt)
+		}
+
+		// This is just the naive approach of carrying forward the previous row,
+		// but I wonder if we could optimize things further by computing the *next*
+		// row as we determine the current row's tiles, which would then feed into
+		// the row after that, and so on. That's maybe more interesting if we're
+		// computing just a single tile from a given row in the far-distant future.
+		// I also didn't spend any time looking for other patterns.
+		row := make([]byte, len(first))
+		for i := range row {
 			st := lib.If(i > 0 && prev[i-1] == trap, left, 0) |
 				lib.If(prev[i] == trap, center, 0) |
 				lib.If(i < len(prev)-1 && prev[i+1] == trap, right, 0)
 			if st == left|center || st == center|right || st == left || st == right {
-				rows[r][i] = trap
+				row[i] = trap
 			} else {
-				rows[r][i] = safe
+				row[i] = safe
+				cnt++
 			}
 		}
+		prev = row
 	}
-	fmt.Println(lib.CountBytesFull(rows, '.'))
+
+	// Part 2
+	fmt.Println(cnt)
 }
