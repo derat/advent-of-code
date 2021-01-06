f733a0af84ce93f1d0f79024f49ce330db00732e
Daniel Erat <dan@erat.org>
2021-01-06 18:02:58 -0400

    2017/05: Part 2.

--- 2017/05/main.go
+++ 2017/05/main.go
@@ -8,14 +8,20 @@ import (
 
 func main() {
 	jumps := lib.InputInts("2017/5")
-	fmt.Println(run(jumps))
+	fmt.Println(run(jumps, false))
+	fmt.Println(run(jumps, true))
 }
 
-func run(jumps []int) int {
+func run(jumps []int, part2 bool) int {
+	jumps = append([]int(nil), jumps...) // preserve orig
 	var cnt int
 	for i := 0; i >= 0 && i < len(jumps); {
 		off := jumps[i]
-		jumps[i]++
+		if part2 && off >= 3 {
+			jumps[i]--
+		} else {
+			jumps[i]++
+		}
 		i += off
 		cnt++
 	}
