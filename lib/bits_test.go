57835d7daa798b49cf2993185f6ff3bc1d27069e
Daniel Erat <dan@erat.org>
2021-01-06 17:58:36 -0400

    lib; Fix bits_test.go.

--- lib/bits_test.go
+++ lib/bits_test.go
@@ -8,17 +8,17 @@ import (
 func TestPackInt_RoundTrip(t *testing.T) {
 	const bits = 11
 	orig := []int{(1 << bits) - 1, 54, 0, 1 << (bits - 1), 85}
-	packed := PackInts(orig, bits)
-	got := UnpackInts(packed, bits, len(orig))
+	packed := PackInts(orig...)
+	got := UnpackInts(packed, len(orig))
 	if !reflect.DeepEqual(got, orig) {
-		t.Errorf("UnpackInts(%#x, %d, %d) = %v; want %v", packed, bits, len(orig), got, orig)
+		t.Errorf("UnpackInts(%#x, %d) = %v; want %v", packed, len(orig), got, orig)
 	}
 }
 
 func TestUnpackIntSigned(t *testing.T) {
-	const bits = 4
 	orig := []int{-7, 5, -1, 0, 7, -2}
-	packed := PackInts(orig, bits)
+	bits := 64 / len(orig)
+	packed := PackInts(orig...)
 	for i, want := range orig {
 		if got := UnpackIntSigned(packed, bits, i); got != want {
 			t.Errorf("UnpackIntSigned(%#b, %d, %d) = %v; want %v", packed, bits, i, got, want)
