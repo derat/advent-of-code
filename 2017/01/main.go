dedc9d84600808a70e199648f6e22dc3db164587
Daniel Erat <dan@erat.org>
2021-01-05 21:04:46 -0400

    2017/01: Part 2.

--- 2017/01/main.go
+++ 2017/01/main.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
-	var sum int
 	digits := lib.ExtractDigits(lib.Input("2017/1"))
+	cnt := len(digits)
+	var sum, sum2 int
 	for i, n := range digits {
-		sum += lib.If(n == digits[(i+1)%len(digits)], n, 0)
+		sum += lib.If(n == digits[(i+1)%cnt], n, 0)
+		sum2 += lib.If(n == digits[(i+cnt/2)%cnt], n, 0)
 	}
 	fmt.Println(sum)
+	fmt.Println(sum2)
 }
