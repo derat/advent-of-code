ae0b3851c97a34e23afa83ca5ab91599a0700688
Daniel Erat <dan@erat.org>
2021-01-05 21:51:26 -0400

    2017/02: Part 2.

--- 2017/02/main.go
+++ 2017/02/main.go
@@ -7,10 +7,22 @@ import (
 )
 
 func main() {
-	var sum int
+	var sum, sum2 int
 	for _, ln := range lib.InputLines("2017/2") {
 		nums := lib.ExtractInts(ln)
 		sum += lib.Max(nums...) - lib.Min(nums...)
+
+		// Part 2
+	Loop:
+		for i, a := range nums {
+			for j, b := range nums {
+				if i != j && a%b == 0 {
+					sum2 += a / b
+					break Loop
+				}
+			}
+		}
 	}
 	fmt.Println(sum)
+	fmt.Println(sum2)
 }
