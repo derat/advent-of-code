a297c3f1df944b2e1cb4454866450780a8943794
Daniel Erat <dan@erat.org>
2021-01-06 12:10:05 -0400

    2017/04: Part 2.

--- 2017/04/main.go
+++ 2017/04/main.go
@@ -2,23 +2,43 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/derat/advent-of-code/lib"
 )
 
 func main() {
-	var nvalid int
-Loop:
+	var nvalid, nvalid2 int
 	for _, ln := range lib.InputLines("2017/4") {
 		seen := make(map[string]struct{})
+		seen2 := make(map[string]struct{}) // Part 2: sorted letters
+
+		valid, valid2 := true, true
 		for _, s := range strings.Fields(ln) {
 			if _, ok := seen[s]; ok {
-				continue Loop
+				valid = false
+			} else {
+				seen[s] = struct{}{}
 			}
-			seen[s] = struct{}{}
+
+			b := []byte(s)
+			sort.Slice(b, func(i, j int) bool { return b[i] < b[j] })
+			ana := string(b)
+			if _, ok := seen2[ana]; ok {
+				valid2 = false
+			} else {
+				seen2[ana] = struct{}{}
+			}
 		}
-		nvalid++
+
+		if valid {
+			nvalid++
+		}
+		if valid2 {
+			nvalid2++
+		}
 	}
 	fmt.Println(nvalid)
+	fmt.Println(nvalid2)
 }
