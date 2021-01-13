a0684ade8a638ba32365ed61dd1a7992e2ac7f83
Daniel Erat <dan@erat.org>
2021-01-13 08:14:28 -0400

    2018/02: Part 2.

--- 2018/02/main.go
+++ 2018/02/main.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
+	words := lib.InputLines("2018/2")
+
+	// Part 1: Multiple counts of words with two and with three duplicate letters.
 	var cnt2, cnt3 int
-	for _, ln := range lib.InputLines("2018/2") {
+	for _, word := range words {
 		cnts := make(map[rune]int, 26)
-		for _, b := range ln {
+		for _, b := range word {
 			cnts[b]++
 		}
 		if lib.MapHasValue(cnts, 2) {
@@ -21,4 +24,18 @@ func main() {
 		}
 	}
 	fmt.Println(cnt2 * cnt3)
+
+	// Part 2: Print common portions of two words that differ by exactly one letter.
+	seen := make(map[string]struct{})
+Loop:
+	for _, word := range words {
+		for i := range word {
+			blank := word[:i] + "_" + word[i+1:]
+			if _, ok := seen[blank]; ok {
+				fmt.Println(word[:i] + word[i+1:])
+				break Loop
+			}
+			seen[blank] = struct{}{}
+		}
+	}
 }
