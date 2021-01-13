f74105e4350448c7bd40b161321dcbc3c0496ba4
Daniel Erat <dan@erat.org>
2021-01-12 21:56:22 -0400

    2018/01: Part 2.

--- 2018/01/main.go
+++ 2018/01/main.go
@@ -8,5 +8,22 @@ import (
 
 func main() {
 	changes := lib.InputInts("2018/1")
+
+	// Part 1: Sum of all changes.
 	fmt.Println(lib.Sum(changes...))
+
+	// Part 2: First value seen twice.
+	var val int
+	seen := map[int]struct{}{0: struct{}{}}
+Loop:
+	for {
+		for _, ch := range changes {
+			val += ch
+			if _, ok := seen[val]; ok {
+				fmt.Println(val)
+				break Loop
+			}
+			seen[val] = struct{}{}
+		}
+	}
 }
