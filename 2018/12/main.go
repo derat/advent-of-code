0ad8ee9b32744fe1da3e5af9d9fd894ec5fd722e
Daniel Erat <dan@erat.org>
2021-01-14 22:11:28 -0400

    2018/12: Part 2.

--- 2018/12/main.go
+++ 2018/12/main.go
@@ -35,32 +35,73 @@ func main() {
 		pats[pat] = struct{}{}
 	}
 
-	for gen := 0; gen < 20; gen++ {
-		newPlants := make(map[int]struct{})
-		seen := make(map[int]struct{})
-		for p := range plants {
-			// Iterate over this plant and the two pots on each side of it.
-			for c := p - 2; c <= p+2; c++ {
-				if _, ok := seen[c]; ok {
-					continue // pot has already been handled
-				}
-				seen[c] = struct{}{}
+	// Map from state to gen and value of leftmost plant.
+	str, left := state(plants)
+	seen := map[string][2]int{str: [2]int{0, left}}
 
-				var pat byte
-				for i := c - 2; i <= c+2; i++ {
-					pat <<= 1
-					if _, ok := plants[i]; ok {
-						pat |= 1
-					}
-				}
-				if _, ok := pats[pat]; ok {
-					newPlants[c] = struct{}{}
-				}
-			}
+	for gen := 1; true; gen++ {
+		plants = update(plants, pats)
+
+		// Part 1: Print sum of pots with plants after 20 generations.
+		if gen == 20 {
+			fmt.Println(lib.Sum(lib.MapIntKeys(plants)...))
 		}
 
-		plants = newPlants
+		// Part 2: Print sum of pots with plants after 30 billion generations.
+		str, left := state(plants)
+		if info, ok := seen[str]; ok {
+			// My input ends up in a 1-generation loop. If the loop included more generations,
+			// we'd need to repeat it as many times as possible and then manually play out the
+			// remaining generations.
+			pgen, pleft := info[0], info[1]
+			lib.AssertEq(1, gen-pgen)
+			shift := int64(left - pleft)       // shift amount each generation
+			rem := 50_000_000_000 - int64(gen) // remaining generations
+			val := int64(lib.Sum(lib.MapIntKeys(plants)...))
+			fmt.Println(val + int64(len(plants))*rem*shift)
+			break
+		}
+		seen[str] = [2]int{gen, left}
+	}
+}
+
+func update(plants map[int]struct{}, pats map[byte]struct{}) map[int]struct{} {
+	newPlants := make(map[int]struct{})
+	done := make(map[int]struct{})
+	for p := range plants {
+		// Iterate over this plant and the two pots on each side of it.
+		for c := p - 2; c <= p+2; c++ {
+			if _, ok := done[c]; ok {
+				continue // pot has already been handled
+			}
+			done[c] = struct{}{}
+
+			var pat byte
+			for i := c - 2; i <= c+2; i++ {
+				pat <<= 1
+				if _, ok := plants[i]; ok {
+					pat |= 1
+				}
+			}
+			if _, ok := pats[pat]; ok {
+				newPlants[c] = struct{}{}
+			}
+		}
 	}
 
-	fmt.Println(lib.Sum(lib.MapIntKeys(plants)...))
+	return newPlants
+}
+
+func state(plants map[int]struct{}) (string, int) {
+	keys := lib.MapIntKeys(plants)
+	min, max := lib.Min(keys...), lib.Max(keys...)
+	b := make([]byte, max-min+1)
+	for i := range b {
+		if _, ok := plants[min+i]; ok {
+			b[i] = '#'
+		} else {
+			b[i] = '.'
+		}
+	}
+	return string(b), min
 }
