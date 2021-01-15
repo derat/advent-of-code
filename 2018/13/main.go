9c63b198f142a9ec4e8f426850967698c75475bb
Daniel Erat <dan@erat.org>
2021-01-15 10:59:42 -0400

    2018/13: Part 2.

--- 2018/13/main.go
+++ 2018/13/main.go
@@ -10,33 +10,36 @@ import (
 func main() {
 	tracks := lib.InputLinesBytes("2018/13", '-', '|', '\\', '/', '+', ' ', '^', 'v', '<', '>')
 
-	var carts []cart
+	var carts []*cart
 	for r, row := range tracks {
 		for c, ch := range row {
 			switch ch {
 			case '^':
-				carts = append(carts, cart{r, c, up, 0})
+				carts = append(carts, &cart{r: r, c: c, dir: up})
 				row[c] = '|'
 			case 'v':
-				carts = append(carts, cart{r, c, down, 0}) // for want of a ternary operator
+				carts = append(carts, &cart{r: r, c: c, dir: down}) // for want of a ternary operator
 				row[c] = '|'
 			case '<':
-				carts = append(carts, cart{r, c, left, 0})
+				carts = append(carts, &cart{r: r, c: c, dir: left})
 				row[c] = '-'
 			case '>':
-				carts = append(carts, cart{r, c, right, 0})
+				carts = append(carts, &cart{r: r, c: c, dir: right})
 				row[c] = '-'
 			}
 		}
 	}
 
-	locs := make(map[uint64]int) // packed r,c of carts
-	for i, ca := range carts {
-		locs[lib.PackInts(ca.r, ca.c)] = i
+	// Sigh, I spent way too long on a bug here.
+	// I was storing cart indexes rather than pointers here,
+	// so things got completely messed up after sorting in the loop.
+	locs := make(map[uint64]*cart) // packed r,c of carts
+	for _, ca := range carts {
+		locs[lib.PackInts(ca.r, ca.c)] = ca
 	}
 
 	var tick int
-Loop:
+	var ncol int
 	for tick = 1; true; tick++ {
 		// Sort carts from top to bottom and left to right.
 		sort.Slice(carts, func(i, j int) bool {
@@ -48,8 +51,11 @@ Loop:
 			return carts[i].c < carts[j].c
 		})
 
-		for i := range carts {
-			ca := &carts[i]
+		for _, ca := range carts {
+			// Skip removed carts.
+			if ca.rem {
+				continue
+			}
 
 			// Turn if needed.
 			switch tracks[ca.r][ca.c] {
@@ -75,24 +81,43 @@ Loop:
 				}
 				ca.ints++
 			case '|':
-				lib.Assertf(ca.dir == up || ca.dir == down, "Cart %d not vert at %d,%d", i, ca.r, ca.c)
+				lib.Assertf(ca.dir == up || ca.dir == down, "Cart not vert at %d,%d", ca.r, ca.c)
 			case '-':
-				lib.Assertf(ca.dir == left || ca.dir == right, "Card %d not horiz at %d,%d", i, ca.r, ca.c)
+				lib.Assertf(ca.dir == left || ca.dir == right, "Cart not horiz at %d,%d", ca.r, ca.c)
 			}
 
 			// Move the cart.
 			delete(locs, lib.PackInts(ca.r, ca.c))
-
 			ca.r += ca.dir.dr()
 			ca.c += ca.dir.dc()
 
 			// Check for collision.
 			pos := lib.PackInts(ca.r, ca.c)
-			if _, ok := locs[pos]; ok {
-				fmt.Printf("%d,%d\n", ca.c, ca.r) // print as X,Y
-				break Loop
+			if other, ok := locs[pos]; ok {
+				// Part 1: Print X,Y of the first collision.
+				ncol++
+				if ncol == 1 {
+					fmt.Printf("%d,%d\n", ca.c, ca.r)
+				}
+
+				// Remove both carts.
+				ca.rem = true
+				other.rem = true
+				delete(locs, pos)
+			} else {
+				locs[pos] = ca
 			}
-			locs[pos] = i
+		}
+
+		// Part 2: Print X,Y of last cart at end of first tick where it's the only one left.
+		if ncol == len(carts)/2 {
+			for _, ca := range carts {
+				if !ca.rem {
+					fmt.Printf("%d,%d\n", ca.c, ca.r)
+					break
+				}
+			}
+			break
 		}
 	}
 }
@@ -100,7 +125,8 @@ Loop:
 type cart struct {
 	r, c int
 	dir  dir
-	ints int // intersection count
+	ints int  // intersection count
+	rem  bool // removed after collision
 }
 
 // Copied from 2017/22.
