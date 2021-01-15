2655680f7c98c4272e2f5e216d1a0e6a0987621c
Daniel Erat <dan@erat.org>
2021-01-15 15:02:01 -0400

    2018/14: Part 2.

--- 2018/14/main.go
+++ 2018/14/main.go
@@ -18,14 +18,25 @@ func main() {
 	}
 
 	var nscores int
-	scores := make([]uint64, (needed+size-1)/size)
+	var scores []uint64
+
+	var seq uint64
+	nseq := len(fmt.Sprint(input))
 
 	add := func(score int) {
 		lib.AssertLess(score, 10)
+
 		idx := nscores / size
 		pos := nscores % size
+		if idx == len(scores) {
+			scores = append(scores, 0)
+		} else if idx > len(scores) {
+			lib.Panicf("Invalid index %d with scores array of len %d", idx, len(scores))
+		}
 		scores[idx] += uint64(score) * pows[pos]
 		nscores++
+
+		seq = (10*seq + uint64(score)) % pows[nseq]
 	}
 
 	get := func(n int) int {
@@ -44,20 +55,35 @@ func main() {
 	add(7)
 	elf1, elf2 := 0, 1
 
-	// Part 1: Print scores of ten recipes immediately after number of recipes in input.
-	for nscores < needed {
+	done := false // printed part 1 answer
+	for {
 		score1, score2 := get(elf1), get(elf2)
 		sum := score1 + score2
 		if sum > 9 {
 			add(1)
+			if seq == uint64(input) {
+				break
+			}
 		}
 		add(sum % 10)
+		if seq == uint64(input) {
+			break
+		}
+
+		// Part 1: Print scores of ten recipes immediately after number of recipes in input.
+		if !done && nscores >= needed {
+			for i := 0; i < 10; i++ {
+				fmt.Print(get(input + i))
+			}
+			fmt.Println()
+			done = true
+		}
 
 		elf1 = (elf1 + 1 + score1) % nscores
 		elf2 = (elf2 + 1 + score2) % nscores
 	}
-	for i := 0; i < 10; i++ {
-		fmt.Print(get(input + i))
-	}
-	fmt.Println()
+
+	// Part 2: Print number of recipes appearing on scoreboard to left of score sequence in input.
+	lib.Assertf(done, "Found part 2 sequence after %d scores; need %d for part 1", nscores, needed)
+	fmt.Println(nscores - nseq)
 }
