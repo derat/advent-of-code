00c3bfe44cedbe5a4c29c975f74417fbad364c7b
Daniel Erat <dan@erat.org>
2021-01-21 14:46:53 -0400

    2019/01: Part 2.

--- 2019/01/main.go
+++ 2019/01/main.go
@@ -7,9 +7,26 @@ import (
 )
 
 func main() {
-	var sum int
+	reqs := make(map[int]int)
+	var req func(int) int
+	req = func(m int) int {
+		if m <= 0 {
+			return 0
+		}
+		if r, ok := reqs[m]; ok {
+			return r
+		}
+		r := lib.Max(m/3-2, 0)
+		r += req(r)
+		reqs[m] = r
+		return r
+	}
+
+	var sum, sum2 int
 	for _, m := range lib.InputInts("2019/1") {
 		sum += m/3 - 2
+		sum2 += req(m)
 	}
 	fmt.Println(sum)
+	fmt.Println(sum2)
 }
