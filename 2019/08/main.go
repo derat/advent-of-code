51210317e5ac4c2b20391f94c164be4d3dfeb9bd
Daniel Erat <dan@erat.org>
2021-01-22 12:02:35 -0400

    2019/08: Part 2.

--- 2019/08/main.go
+++ 2019/08/main.go
@@ -25,6 +25,7 @@ func main() {
 		}
 	}
 
+	// Part 1: Print number of 1s times number of 2s from layer with fewest 0s.
 	minZeros := math.MaxInt32
 	prod := 0
 	for _, l := range layers {
@@ -35,4 +36,32 @@ func main() {
 		}
 	}
 	fmt.Println(prod)
+
+	// Part 2: Stack layers: first in front; 0 is black, 1 is white, 2 is transparent.
+	img := lib.NewBytes(nrows, ncols, ' ')
+	for r, row := range img {
+		for c := range row {
+		Loop:
+			for i, l := range layers {
+				ch := l[r][c]
+				switch ch {
+				case '0': // black
+					row[c] = ' '
+					break Loop
+				case '1': // white
+					row[c] = '#'
+					break Loop
+				case '2': // transparent
+				default:
+					lib.Panicf("Invalid char %q at %v,%v in layer %v", ch, r, c, i)
+				}
+			}
+		}
+	}
+	//fmt.Println(lib.DumpBytes(img))
+	// Hack: lib.OCR expects a blank column between letters, but
+	// my input has the second letter flush against the third letter.
+	left := lib.OCR(lib.CopyBytesRegion(img, 0, 0, nrows-1, 11), ' ')
+	right := lib.OCR(lib.CopyBytesRegion(img, 0, 12, nrows-1, ncols-1), ' ')
+	fmt.Println(left + right)
 }
