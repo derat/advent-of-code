9decde01cd580cacf108fbb254d0054b9438359b
Daniel Erat <dan@erat.org>
2021-01-21 21:43:29 -0400

    2019/06: Part 2.

--- 2019/06/main.go
+++ 2019/06/main.go
@@ -14,7 +14,7 @@ func main() {
 		orbits[a] = append(orbits[a], b)
 	}
 
-	root := &node{"COM", nil}
+	root := &node{"COM", nil, nil}
 	nodes := map[string]*node{root.name: root}
 	next := []string{root.name}
 
@@ -24,13 +24,14 @@ func main() {
 		n := nodes[name]
 		lib.Assert(n != nil)
 		for _, cname := range orbits[name] {
-			c := &node{name: cname}
+			c := &node{name: cname, parent: n}
 			nodes[cname] = c
 			n.children = append(n.children, c)
 			next = append(next, cname)
 		}
 	}
 
+	// Part 1: Print total number of direct and indirect orbits.
 	var count func(*node, int) int
 	count = func(n *node, depth int) int {
 		sum := depth // count ourselves
@@ -40,9 +41,23 @@ func main() {
 		return sum
 	}
 	fmt.Println(count(root, 0))
+
+	// Part 2: Print number of orbital transfers needed to go from YOU to SAN.
+	// (Really seems like the distance from YOU's parent to SAN's parent, though.)
+	ancestors := make(map[string]int)
+	for n, c := nodes["YOU"].parent, 0; n != nil; n, c = n.parent, c+1 {
+		ancestors[n.name] = c
+	}
+	for n, c := nodes["SAN"].parent, 0; n != nil; n, c = n.parent, c+1 {
+		if v, ok := ancestors[n.name]; ok {
+			fmt.Println(c + v)
+			break
+		}
+	}
 }
 
 type node struct {
 	name     string
 	children []*node
+	parent   *node
 }
