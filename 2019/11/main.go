55ac7c8ac62691ac73baa527d802a2090a083ddd
Daniel Erat <dan@erat.org>
2021-01-23 11:28:16 -0400

    2019/11: Part 2.

--- 2019/11/main.go
+++ 2019/11/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/derat/advent-of-code/lib"
 )
@@ -10,7 +11,39 @@ func main() {
 	input := lib.InputInt64s("2019/11")
 
 	// Part 1: How many panels get painted at least once?
-	vm := lib.NewIntcode(input)
+	panels := make(map[uint64]int64)
+	run(input, panels)
+	fmt.Println(len(panels))
+
+	// Part 2: What's the 8-letter registration identifier that's
+	// painted after starting on a single white panel?
+	panels = map[uint64]int64{lib.PackInts(0, 0): 1}
+	run(input, panels)
+
+	rmin, rmax := math.MaxInt32, math.MinInt32
+	cmin, cmax := math.MaxInt32, math.MinInt32
+	for p := range panels {
+		r, c := lib.UnpackIntSigned2(p)
+		rmin = lib.Min(rmin, r)
+		rmax = lib.Max(rmax, r)
+		cmin = lib.Min(cmin, c)
+		cmax = lib.Max(cmax, c)
+	}
+	nrows := rmax - rmin + 1
+	ncols := cmax - cmin + 1
+	grid := lib.NewBytes(nrows, ncols, ' ')
+	for p, v := range panels {
+		r, c := lib.UnpackIntSigned2(p)
+		if v == 1 {
+			grid[r][c] = '#'
+		}
+	}
+	//fmt.Println(lib.DumpBytes(grid))
+	fmt.Println(lib.OCR(grid, ' '))
+}
+
+func run(prog []int64, panels map[uint64]int64) {
+	vm := lib.NewIntcode(prog)
 
 	// Use unbuffered channels.
 	vm.In = make(chan int64)
@@ -20,9 +53,8 @@ func main() {
 
 	var r, c int
 	dir := lib.Up
-	painted := make(map[uint64]int64)
-	var color int64  // current color (0 is black, 1 is white)
-	painting := true // waiting for paint command (as opposed to turn)
+	color := panels[lib.PackInts(r, c)] // current color (0 is black, 1 is white)
+	painting := true                    // waiting for paint command (as opposed to turn)
 
 Loop:
 	for {
@@ -32,7 +64,7 @@ Loop:
 				break Loop
 			}
 			if painting {
-				painted[lib.PackInts(r, c)] = v
+				panels[lib.PackInts(r, c)] = v
 			} else { // turning
 				switch v {
 				case 0:
@@ -46,10 +78,9 @@ Loop:
 				c += dir.DC()
 			}
 			painting = !painting
-			color = painted[lib.PackInts(r, c)]
+			color = panels[lib.PackInts(r, c)]
 		case vm.In <- color:
 		}
 	}
 	lib.Assert(vm.Wait())
-	fmt.Println(len(painted))
 }
