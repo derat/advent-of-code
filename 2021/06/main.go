67eafb01de782a105f67770adea6e1fb7f6db498
Daniel Erat <dan@erat.org>
2021-12-06 08:04:35 -0400

    2021/06: Parts 1 and 2. [ci skip]

---
+++ 2021/06/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/derat/advent-of-code/lib"
+)
+
+func main() {
+	var cnt1, cnt2 int64
+	for _, state := range lib.InputInts("2021/6") {
+		cnt1 += 1 + numProd(state, 80)
+		cnt2 += 1 + numProd(state, 256)
+	}
+	fmt.Println(cnt1)
+	fmt.Println(cnt2)
+}
+
+var numProdCache = make(map[uint64]int64)
+
+func numProd(state, days int) int64 {
+	key := lib.PackInts(state, days)
+	if v, ok := numProdCache[key]; ok {
+		return v
+	}
+
+	var cnt int64
+	for days = days - state - 1; days >= 0; days -= 7 {
+		cnt += 1 + numProd(8, days)
+	}
+	numProdCache[key] = cnt
+	return cnt
+}
