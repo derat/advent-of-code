71d153e472c3d5398a330e9aa314d09056e9d664
Daniel Erat <dan@erat.org>
2021-12-07 09:27:19 -0400

    2021/07: Add simpler approach for part 2. [ci skip]

--- 2021/07/main.go
+++ 2021/07/main.go
@@ -58,6 +58,19 @@ func main() {
 		min = lib.Min(min, left[i]+right[i])
 	}
 	fmt.Println(min)
+
+	// Sigh, much simpler approach: the cost of moving from p to t is the summation
+	// from 1 to the absolute value of distance d, which is just d*(d+1)/2.
+	min2 := math.MaxInt32
+	for t := 0; t <= max; t++ {
+		var cost int
+		for _, p := range initial {
+			d := lib.Abs(p - t)
+			cost += d * (d + 1) / 2
+		}
+		min2 = lib.Min(min2, cost)
+	}
+	lib.AssertEq(min2, min)
 }
 
 func slowCost(initial []int, target int) int {
