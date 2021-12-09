d473c9e1935d742ff3b169cdb05c12875f82f90e
Daniel Erat <dan@erat.org>
2021-12-09 13:08:08 -0400

    2021/09: Simplify a check in part 2. [ci skip]

--- 2021/09/main.go
+++ 2021/09/main.go
@@ -27,11 +27,10 @@ func main() {
 	var fill func(r, c int) int
 	fill = func(r, c int) int {
 		area := 1
-		h := grid[r][c]
+		used[r][c] = 1
 		for _, p := range [][2]int{{r - 1, c}, {r + 1, c}, {r, c - 1}, {r, c + 1}} {
 			pr, pc := p[0], p[1]
-			if ph := grid.Get(pr, pc, '9'); ph >= h && ph != '9' && used.Get(pr, pc, 0) == 0 {
-				used[pr][pc] = 1
+			if h := grid.Get(pr, pc, '9'); h != '9' && used.Get(pr, pc, 0) == 0 {
 				// Recursion seems to work fine for my input, but another option would
 				// be to keep a 'frontier' list of points and loop until it's empty.
 				area += fill(pr, pc)
@@ -42,8 +41,30 @@ func main() {
 
 	var areas []int
 	for _, p := range lows {
-		r, c := p[0], p[1]
-		areas = append(areas, fill(r, c))
+		// I think that this 'used' check to make sure that the low point hasn't already been used
+		// in a basin isn't necessary. The problem text says:
+		//
+		// "A basin is all locations that eventually flow downward to a single low point. Therefore,
+		// every low point has a basin, although some basins are very small. Locations of height 9
+		// do not count as being in any basin, and all other locations will always be part of
+		// exactly one basin."
+		//
+		// In other words, I don't think that the input can contain a basin with multiple low
+		// points:
+		//
+		//  99999
+		//  92329
+		//  99999
+		//
+		// Also note that input like this apparently doesnt't contain any low points (and therefore
+		// also doesn't contain any basins):
+		//
+		//  9999
+		//  9229
+		//  9999
+		if r, c := p[0], p[1]; used.Get(r, c, 0) == 0 {
+			areas = append(areas, fill(r, c))
+		}
 	}
 	lib.AssertLess(2, len(areas))
 	sort.Sort(sort.Reverse(sort.IntSlice(areas)))
