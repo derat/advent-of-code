f6f2a89dc02150cc53d605aba4179cef73531db9
Daniel Erat <dan@erat.org>
2021-12-11 10:00:26 -0400

    2021/11: Add mediocre visualization.
    I changed my approach to be non-recursive so I'd be able to
    draw the state after each flash.

--- 2021/11/main.go
+++ 2021/11/main.go
@@ -2,28 +2,52 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/derat/advent-of-code/lib"
+)
+
+const (
+	animate = false
+	delay   = 50 * time.Millisecond
 )
 
 func main() {
 	grid := lib.InputByteGrid("2021/11")
 	//fmt.Println("Before any steps:")
 	//fmt.Println(grid.Dump())
+	if animate {
+		fmt.Print(lib.ClearScreen)
+	}
 
 	var nflashes int
-	var inc func(r, c int)
-	inc = func(r, c int) {
-		// "First, the energy level of each octopus increases by 1."
-		grid[r][c]++
+	for step := 1; true; step++ {
+		old := nflashes
+		var pending [][2]int // pending flashes from this round
 
-		// "Then, any octopus with an energy level greater than 9 flashes. This increases the
-		// energy level of all adjacent octopuses by 1, including octopuses that are diagonally
-		// adjacent. If this causes an octopus to have an energy level greater than 9, it also
-		// flashes. This process continues as long as new octopuses keep having their energy
-		// level increased beyond 9. (An octopus can only flash at most once per step.)"
-		if grid[r][c] == '9'+1 {
-			nflashes++
+		inc := func(r, c int) {
+			grid[r][c]++
+			if grid[r][c] == '9'+1 {
+				nflashes++
+				pending = append(pending, [2]int{r, c})
+			}
+		}
+
+		// "First, the energy level of each octopus increases by 1."
+		grid.Iter(inc)
+		if animate {
+			dump(grid)
+		}
+
+		// "Then, any octopus with an energy level greater than 9 flashes. This increases the energy
+		// level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent.
+		// If this causes an octopus to have an energy level greater than 9, it also flashes. This
+		// process continues as long as new octopuses keep having their energy level increased
+		// beyond 9. (An octopus can only flash at most once per step.)"
+		for len(pending) > 0 {
+			r, c := pending[0][0], pending[0][1]
+			pending = pending[1:]
+
 			for dr := -1; dr <= 1; dr++ {
 				for dc := -1; dc <= 1; dc++ {
 					if nr, nc := r+dr, c+dc; grid.InBounds(nr, nc) && !(nr == r && nc == c) {
@@ -32,11 +56,9 @@ func main() {
 				}
 			}
 		}
-	}
-
-	for step := 1; true; step++ {
-		old := nflashes
-		grid.Iter(inc)
+		if animate && nflashes > old {
+			dump(grid)
+		}
 
 		// "Finally, any octopus that flashed during this step has its energy level set to 0,
 		// as it used all of its energy to flash."
@@ -50,14 +72,49 @@ func main() {
 		//fmt.Println(grid.Dump())
 
 		// Part 1: "How many total flashes are there after 100 steps?"
-		if step == 100 {
+		if step == 100 && !animate {
 			fmt.Println(nflashes)
 		}
 
 		// Part 2: "What is the first step during which all octopuses flash?"
-		if nflashes == old+100 {
+		if nflashes == old+100 && !animate {
 			fmt.Println(step)
 			break
 		}
 	}
+}
+
+var (
+	colors = []string{
+		lib.Color(233),
+		lib.Color(235),
+		lib.Color(238),
+		lib.Color(241),
+		lib.Color(244),
+		lib.Color(246),
+		lib.Color(248),
+		lib.Color(250),
+		lib.Color(252),
+		lib.Color(254),
+	}
+	flashColor = lib.Color(11)
+)
+
+func dump(grid lib.ByteGrid) {
+	var s string
+	s += lib.MoveHome
+	for r, row := range grid {
+		if r != 0 {
+			s += "\n"
+		}
+		for _, ch := range row {
+			if i := ch - '0'; i >= 0 && int(i) < len(colors) {
+				s += colors[i] + string(ch) + " "
+			} else {
+				s += flashColor + "█ "
+			}
+		}
+	}
+	fmt.Print(s)
+	time.Sleep(delay)
 }
---
+++ lib/ansi.go
@@ -0,0 +1,17 @@
+package lib
+
+import "fmt"
+
+// Color returns the ANSI escape sequence for setting the foreground color to c.
+// See https://stackoverflow.com/a/33206814 for available colors.
+func Color(c uint8) string {
+	return fmt.Sprintf("\033[38;5;%dm", c)
+}
+
+const (
+	// ClearScreen clears the screen.
+	ClearScreen = "\033[2J"
+	// MoveHome moves the cursor to the top-left corner of the screen.
+	// Writing to stdout will overwrite the existing contents of the screen.
+	MoveHome = "\033[H"
+)
