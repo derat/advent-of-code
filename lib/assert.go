fb9135113f9a5d0d93ba58a9b7808fa8a6751cfe
Daniel Erat <dan@erat.org>
2021-12-21 08:11:05 -0400

    lib: Make AssertEq a bit smarter about different types.

--- lib/assert.go
+++ lib/assert.go
@@ -1,6 +1,9 @@
 package lib
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // Assert panics if v is false.
 func Assert(v bool) {
@@ -23,8 +26,65 @@ func Panicf(s string, args ...interface{}) {
 
 // AssertEq panics if a doesn't equal b.
 func AssertEq(a, b interface{}) {
-	if a != b {
+	var eq bool
+	if av, ok := convertInt(a); ok {
+		if bv, ok := convertInt(b); ok {
+			eq = av == bv
+		} else {
+			panic(fmt.Sprintf("%v not convertible to int64", reflect.TypeOf(b)))
+		}
+	} else if av, ok := convertFloat(a); ok {
+		if bv, ok := convertFloat(b); ok {
+			eq = av == bv
+		} else {
+			panic(fmt.Sprintf("%v not convertible to float64", reflect.TypeOf(b)))
+		}
+	} else if k := reflect.TypeOf(a).Kind(); k == reflect.Slice || k == reflect.Map {
+		eq = reflect.DeepEqual(a, b)
+	} else {
+		eq = a == b
+	}
+
+	if !eq {
 		panic(fmt.Sprintf("%v != %v", a, b))
+	}
+}
+
+func convertInt(i interface{}) (int64, bool) {
+	switch v := i.(type) {
+	case int:
+		return int64(v), true
+	case int8:
+		return int64(v), true
+	case int16:
+		return int64(v), true
+	case int32:
+		return int64(v), true
+	case int64:
+		return v, true
+	case uint:
+		return int64(v), true
+	case uint8:
+		return int64(v), true
+	case uint16:
+		return int64(v), true
+	case uint32:
+		return int64(v), true
+	case uint64:
+		return int64(v), true
+	default:
+		return 0, false
+	}
+}
+
+func convertFloat(i interface{}) (float64, bool) {
+	switch v := i.(type) {
+	case float32:
+		return float64(v), true
+	case float64:
+		return v, true
+	default:
+		return 0, false
 	}
 }
 
---
+++ lib/assert_test.go
@@ -0,0 +1,49 @@
+package lib
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestAssertEq(t *testing.T) {
+	for _, tc := range []struct {
+		a, b interface{}
+		eq   bool
+	}{
+		{true, true, true},
+		{true, false, false},
+		{3, 3, true},
+		{3, 2, false},
+		{'a', 'a', true},
+		{int64(3), int64(3), true},
+		{int(3), int64(3), true},
+		{byte('a'), 'a', true},
+		{byte('a'), rune('a'), true},
+		{3.5, 3.5, true},
+		{3.5, 3.4, false},
+		{float32(3.5), float64(3.5), true},
+		{"abc", "abc", true},
+		{"abc", "def", false},
+		{[]int{}, []int{}, true},
+		{[]int{2, 3}, []int{2, 3}, true},
+		{[]int{2, 3}, []int{3, 2}, false},
+		{[]int{}, []int(nil), false},
+		{map[int]int{}, map[int]int{}, true},
+		{map[int]int{1: 2}, map[int]int{1: 2}, true},
+		{map[int]int{1: 2}, map[int]int{2: 1}, false},
+		{map[int]int{}, map[int]int(nil), false},
+	} {
+		t.Run(fmt.Sprintf("%v (%v), %v (%v)", tc.a, reflect.TypeOf(tc.a), tc.b, reflect.TypeOf(tc.b)),
+			func(t *testing.T) {
+				defer func() {
+					if r := recover(); r != nil && tc.eq {
+						t.Errorf("AssertEq panicked: %s", r)
+					} else if r == nil && !tc.eq {
+						t.Error("AssertEq unexpectedly passed")
+					}
+				}()
+				AssertEq(tc.a, tc.b)
+			})
+	}
+}
