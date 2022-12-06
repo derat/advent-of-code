474ad23a92cb04bf87f65610ca2006a744566e30
Daniel Erat <dan@erat.org>
2022-12-06 15:58:58 -0300

    2022/06: Loop to share code.

--- 2022/06/main.go
+++ 2022/06/main.go
@@ -8,44 +8,24 @@ import (
 
 func main() {
 	input := lib.Input("2022/6")
-
-	// Part 1: 1-based index of start of first sequence of 4 unique chars.
-	counts := make(map[byte]int)
-	start := -1
-	for i := range input {
-		counts[input[i]]++
-		if i >= 4 {
-			sub(counts, input[i-4])
+	for _, size := range []int{4, 14} {
+		counts := make(map[byte]int)
+		start := -1
+		for i := range input {
+			counts[input[i]]++
+			if i >= size {
+				if old := input[i-size]; counts[old] > 1 {
+					counts[old]--
+				} else {
+					delete(counts, old)
+				}
+			}
+			if len(counts) == size {
+				start = i
+				break
+			}
 		}
-		if len(counts) == 4 {
-			start = i
-			break
-		}
-	}
-	lib.Assert(start >= 0)
-	fmt.Println(start + 1)
-
-	// Part 2: 14 unique chars.
-	counts2 := make(map[byte]int)
-	start2 := -1
-	for i := range input {
-		counts2[input[i]]++
-		if i >= 14 {
-			sub(counts2, input[i-14])
-		}
-		if len(counts2) == 14 {
-			start2 = i
-			break
-		}
-	}
-	lib.Assert(start2 >= 0)
-	fmt.Println(start2 + 1)
-}
-
-func sub(m map[byte]int, ch byte) {
-	if m[ch] > 1 {
-		m[ch]--
-	} else {
-		delete(m, ch)
+		lib.Assert(start >= 0)
+		fmt.Println(start + 1)
 	}
 }
