644b072b3ee05cddb94e457382308e3d1601e4af
Daniel Erat <dan@erat.org>
2022-12-18 07:33:07 -0300

    2022/18: Parts 1 and 2.

---
+++ 2022/18/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/derat/advent-of-code/lib"
+)
+
+func main() {
+	cubes := make(map[point]struct{})
+	min := point{math.MaxInt, math.MaxInt, math.MaxInt}
+	max := point{math.MinInt, math.MinInt, math.MinInt}
+	for _, ln := range lib.InputLines("2022/18") {
+		vals := lib.ExtractInts(ln)
+		lib.AssertEq(len(vals), 3)
+		p := point{vals[0], vals[1], vals[2]}
+		cubes[p] = struct{}{}
+		min.x, max.x = lib.Min(min.x, p.x), lib.Max(max.x, p.x)
+		min.y, max.y = lib.Min(min.y, p.y), lib.Max(max.y, p.y)
+		min.z, max.z = lib.Min(min.z, p.z), lib.Max(max.z, p.z)
+	}
+
+	// Part 1: "What is the surface area of your scanned lava droplet?"
+	var area int
+	sides := []point{{-1, 0, 0}, {1, 0, 0}, {0, -1, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1}}
+	for c := range cubes {
+		for _, o := range sides {
+			if !lib.MapHasKey(cubes, c.add(o)) {
+				area++
+			}
+		}
+	}
+	fmt.Println(area)
+
+	// Part 2: "What is the exterior surface area of your scanned lava droplet?"
+	var surface int
+	vis := make(map[point]struct{})
+	var grow func(point)
+	grow = func(start point) {
+		if lib.MapHasKey(vis, start) {
+			return // already visited
+		}
+		vis[start] = struct{}{}
+		for _, o := range sides {
+			p := start.add(o)
+			if p.x < min.x || p.x > max.x || p.y < min.y || p.y > max.y || p.z < min.z || p.z > max.z {
+				continue // outside the droplet's bounds
+			}
+			if lib.MapHasKey(cubes, p) {
+				surface++ // occupied; just count the side
+			} else {
+				grow(p) // visit neighbor
+			}
+		}
+	}
+
+	// Mind-numbingly iterate over the cubes on the sides just outside the droplet's bounds.
+	// This overlaps at the edges and corners, but that's fine; grow() avoids visiting
+	// the same cube more than once.
+	bmin := min.add(point{-1, -1, -1})
+	bmax := max.add(point{1, 1, 1})
+	for _, r := range [][6]int{
+		{bmin.x, bmax.x, bmin.y, bmax.y, bmin.z, bmin.z}, // bmin.z
+		{bmin.x, bmax.x, bmin.y, bmax.y, bmax.z, bmax.z}, // bmax.z
+		{bmin.x, bmax.x, bmin.y, bmin.y, bmin.z, bmax.z}, // bmin.y
+		{bmin.x, bmax.x, bmax.y, bmax.y, bmin.z, bmax.z}, // bmax.y
+		{bmin.x, bmin.x, bmin.y, bmax.y, bmin.z, bmax.z}, // bmin.x
+		{bmax.x, bmax.x, bmin.y, bmax.y, bmin.z, bmax.z}, // bmax.x
+	} {
+		for x := r[0]; x <= r[1]; x++ {
+			for y := r[2]; y <= r[3]; y++ {
+				for z := r[4]; z <= r[5]; z++ {
+					grow(point{x, y, z})
+				}
+			}
+		}
+	}
+	fmt.Println(surface)
+}
+
+type point struct{ x, y, z int }
+
+func (p *point) add(o point) point { return point{p.x + o.x, p.y + o.y, p.z + o.z} }
