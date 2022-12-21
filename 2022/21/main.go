ffb0738957b8b001e5ae4aac08981cfe22c42529
Daniel Erat <dan@erat.org>
2022-12-21 09:54:17 -0500

    2022/21: Store pointers to operands (still just part 1).

--- 2022/21/main.go
+++ 2022/21/main.go
@@ -8,35 +8,41 @@ import (
 
 func main() {
 	monkeys := make(map[string]*monkey)
+	children := make(map[*monkey][2]string)
 	const namePat = `[a-z]{4}`
 	for _, ln := range lib.InputLines("2022/21") {
-		var name string
 		var m monkey
+		var lhs, rhs string
 		switch {
 		case lib.TryExtract(ln, `^(`+namePat+`): (`+namePat+`) ([-+*/]) (`+namePat+`)`,
-			&name, &m.lhs, &m.op, &m.rhs):
-		case lib.TryExtract(ln, `^(`+namePat+`): (\d+)$`, &name, &m.val):
+			&m.name, &lhs, &m.op, &rhs):
+			children[&m] = [2]string{lhs, rhs}
+		case lib.TryExtract(ln, `^(`+namePat+`): (\d+)$`, &m.name, &m.val):
 		default:
 			lib.Panicf("Bad line %q", ln)
 		}
-		monkeys[name] = &m
+		monkeys[m.name] = &m
+	}
+	for m, ch := range children {
+		m.lhs = monkeys[ch[0]]
+		m.rhs = monkeys[ch[1]]
 	}
 
-	fmt.Println(monkeys["root"].yell(monkeys))
+	fmt.Println(monkeys["root"].yell())
 }
 
 type monkey struct {
-	val      int64  // 0 if not yet computed
-	lhs, rhs string // monkey names
-	op       byte   // '+', '-', '*', '/', or 0 for literal
+	name     string
+	val      int64 // 0 if not yet computed
+	lhs, rhs *monkey
+	op       byte // '+', '-', '*', '/', or 0 for literal
 }
 
-func (m *monkey) yell(all map[string]*monkey) int64 {
+func (m *monkey) yell() int64 {
 	if m.val != 0 {
 		return m.val // literal or already cached
 	}
-	lhs := all[m.lhs].yell(all)
-	rhs := all[m.rhs].yell(all)
+	lhs, rhs := m.lhs.yell(), m.rhs.yell()
 	switch m.op {
 	case '+':
 		m.val = lhs + rhs
